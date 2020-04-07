6c81f28dc1b1b206666b778caad343f501b7a71a
Jason Moore <JAMoore2@student.fullsail.edu>
2020-04-07 19:43:27 -0400

    does not compile

---
+++ app/src/main/java/com/example/stablelyfe/SLLogin.kt
@@ -0,0 +1,12 @@
+package com.example.stablelyfe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SLLogin : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_s_l_login)
+    }
+}
